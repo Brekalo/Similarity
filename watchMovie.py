0b25c07e3a8e2b636537bb43802c3a99ed5cae61
Vesna <projectappvesna@gmail.com>
2023-05-14 09:17:11 +0100

    Add the updated file

--- watchMovie.py
+++ watchMovie.py
@@ -1,6 +1,9 @@
 import spacy
 
 def preprocess_text(text, nlp):
+    if not isinstance(text, str):
+        raise TypeError("You must enter a string of text")
+    
     # parsed the text using the 'spaCy'
     parsed_text = nlp(text)
 
@@ -9,7 +12,6 @@ def preprocess_text(text, nlp):
         token.lemma_.lower() for token in parsed_text
         if not token.is_stop and not token.is_punct
     ]
-
     return preprocessed_words
 
 
@@ -45,3 +47,11 @@ with open("movies.txt", "r") as my_file:
     movies = my_file.readlines()
 movie_descriptions = [desc.split(":")[1].strip() for desc in movies]
 movie_titles = [desc.split(":")[0] for desc in movies]
+
+given_movie_description = "When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk lands on the planet Sakaar where he is sold into slavery and trained as a gladiator."
+
+similar_movie_title, similar_movie_description = find_similar_movie(
+    given_movie_description, movie_descriptions, movie_titles, nlp)
+
+print(f"\nWe recommend watching this movie: \"{similar_movie_title}\"")
+print(f"The description of the recommended movie is: \"{similar_movie_description}\"")
